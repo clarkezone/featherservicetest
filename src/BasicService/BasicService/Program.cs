77866d1204f9e994545e1a5680a7be426703ff1d
James Clarke <james@clarkezone.net>
2021-05-19 10:38:37 -0700

    Add loki logging via SeriLog

--- src/BasicService/BasicService/Program.cs
+++ src/BasicService/BasicService/Program.cs
@@ -3,12 +3,50 @@ using GreeterLogic;
 using Grpc.Core;
 using GRPC;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Loki;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+
+builder.Configuration.AddEnvironmentVariables();
+builder.Configuration.AddCommandLine(args);
+
+// TODO replace Serilog with Otel->Loki
+// TODO how to change log levels in prod
+builder.Host.UseSerilog((context, configuration)
+	=>
+        {
+            var credentials = new NoAuthCredentials("http://localhost:3100"); //TODO replace with configuration
+            configuration
+                .Enrich
+                .FromLogContext()
+                .Enrich
+                .WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                .Enrich
+                .WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+
+            if (context.HostingEnvironment.IsEnvironment("Production"))
+            {
+                configuration
+                .WriteTo
+                .LokiHttp(credentials);
+            }
+            else
+            {
+                configuration
+                .WriteTo
+                .LokiHttp(credentials)
+                .WriteTo
+                .Console();
+            }
+        }
+	);
 
 var app = builder.Build();
 
@@ -19,24 +57,29 @@ var app = builder.Build();
 app.Listen("https://contoso.com:3001");
 app.MapGrpcService<GreeterService>();
 
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
 await app.RunAsync();
 
 public class GreeterService : Greeter.GreeterBase
 {
-    private readonly ILogger<GreeterService> _logger;
-    private readonly GreeterProvider _greeter;
+	private readonly ILogger<GreeterService> _logger;
+	private readonly GreeterProvider _greeter;
 
-    public GreeterService(ILogger<GreeterService> logger)
-    {
-        _logger = logger;
-        _greeter = new GreeterProvider();
-    }
+	public GreeterService(ILogger<GreeterService> logger)
+	{
+		_logger = logger;
+		_greeter = new GreeterProvider();
+	}
 
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = _greeter.SayHello(request.Name)
-        }); ; ;
-    }
+	public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+	{
+		return Task.FromResult(new HelloReply
+		{
+			Message = _greeter.SayHello(request.Name)
+		}); ; ;
+	}
 }
