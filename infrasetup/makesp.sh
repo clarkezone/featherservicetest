8fdcdc132524a5f6269304779222ae240aa76bc2
James Clarke <james@clarkezone.net>
2021-11-04 21:40:52 +0000

    Fix makesp script

--- infrasetup/makesp.sh
+++ infrasetup/makesp.sh
@@ -6,6 +6,7 @@ az group create -g "$RGNAME" -l "West US"
 
 CREDS=$(az ad sp create-for-rbac --name "$SPNAME" --sdk-auth --role contributor \
 	--scopes /subscriptions/$SUBID/resourceGroups/$RGNAME)
-gh secret set AZURE_CREDENTIALS -b $CREDS
+SHRED=$(echo $CREDS | jq '.clientSecret' --raw-output)
+gh secret set AZURE_CREDENTIALS -b $SHRED
 gh secret set SUBSCRIPTION_ID -b $SUBID
 gh secret set RESOURCE_GROUP -b $RGNAME
