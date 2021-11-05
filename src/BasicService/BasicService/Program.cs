781a1028ca610b8a08c5e75c1cf34fb51b4ba4ea
James Clarke <james@clarkezone.net>
2021-11-05 03:24:04 +0000

    Force build

--- src/BasicService/BasicService/Program.cs
+++ src/BasicService/BasicService/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddGrpc();
 builder.Configuration.AddEnvironmentVariables(prefix:"basicservice_");
 builder.Configuration.AddCommandLine(args);
 
-// TODO replace Serilog with Otel->Loki   
+// TODO replace Serilog with Otel->Loki  
 // TODO how to change log levels in prod 
 builder.Host.UseSerilog((context, configuration)
 	=>
